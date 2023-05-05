611889782840d9c905984333c18ab53788b02c37
Sudeep Suryavanshi <132658680+Sudeep89@users.noreply.github.com>
2023-05-05 15:11:39 +0530

    Add files via upload
    This is the code of fetch api of ccountry data

--- app.js
+++ app.js
@@ -1,27 +1,43 @@
-const outputEl = document.querySelector(".text");
-const valEl = document.querySelectorAll(".val");
-const ClearEl = document.querySelector(".AC");
-const deleteEl = document.querySelector(".DEL");
-const equalEl = document.querySelector(".equal");
+const countryInp = document.getElementById("countryinp");
+const search = document.getElementById("searchbtn");
+const details = document.getElementById("search_details");
+const flag = document.getElementById("flag-icon");
+const Name = document.getElementById("Name");
+const Capital = document.getElementById("capital");
+const Continent = document.getElementById("continent");
+const Population = document.getElementById("population");
+const Currency = document.getElementById("currency");
+const Languages = document.getElementById("language");
 
-valEl.forEach((val) => {
-  val.addEventListener("click", () => {
-    outputEl.value += val.value;
-  });
-});
+async function countryDetails(country) {
+  const finalURL = `https://restcountries.com/v3.1/name/${country}?fullText=true`;
+  const response = await fetch(finalURL);
 
-equalEl.addEventListener("click", () => {
-  try {
-    outputEl.value = eval(outputEl.value);
-  } catch (error) {
-    alert(error);
+  if (response.status === 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector("#search_details").style.display = "none";
+  } else {
+    var data = await response.json();
+    console.log(data);
+
+    flag.src = `${data[0].flags.svg}`;
+    Name.innerHTML = `${data[0].name.common}`;
+    Capital.innerHTML = `${data[0].capital[0]}`;
+    Continent.innerHTML = `${data[0].continents[0]}`;
+    Population.innerHTML = `${data[0].population}`;
+    Currency.innerHTML = `${
+      data[0].currencies[Object.keys(data[0].currencies)].name
+    }- ${Object.keys(data[0].currencies)[0]}`;
+    Languages.innerHTML = `${Object.values(data[0].languages)
+      .toString()
+      .split(",")
+      .join(",")}`;
+
+    document.querySelector("#search_details").style.display = "block";
+    document.querySelector(".error").style.display = "none";
   }
-});
+}
 
-ClearEl.addEventListener("click", () => {
-  outputEl.value = "";
-});
-
-deleteEl.addEventListener("click", () => {
-  outputEl.value = outputEl.value.slice(0, -1);
+search.addEventListener("click", () => {
+  countryDetails(countryInp.value);
 });
